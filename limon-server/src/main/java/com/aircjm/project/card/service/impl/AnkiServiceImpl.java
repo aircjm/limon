0c4590c8b8f0207a6bd3b2d21f674e915b0dbab8
aircjm <aircjm@gmail.com>
2020-07-12 19:51:59 +0800

    对应卡片的管理

--- limon-server/src/main/java/com/aircjm/project/card/service/impl/AnkiServiceImpl.java
+++ limon-server/src/main/java/com/aircjm/project/card/service/impl/AnkiServiceImpl.java
@@ -1,7 +1,6 @@
 package com.aircjm.project.card.service.impl;
 
 import com.aircjm.project.anki.ankiconnect.AnkiVo;
-import com.aircjm.project.anki.ankiconnect.NoteVo;
 import com.aircjm.project.anki.response.AnkiRespVo;
 import com.aircjm.project.card.service.AnkiService;
 import com.aircjm.project.card.vo.anki.vo.Note;
@@ -16,8 +15,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -35,7 +32,7 @@ public class AnkiServiceImpl implements AnkiService {
     private static final String ANKI_URL = "http://127.0.0.1:8765";
 
     @Override
-    public AnkiRespVo post(AnkiVo ankiVo) {
+    public AnkiRespVo postAnki(AnkiVo ankiVo) {
         HttpHeaders headers = new HttpHeaders();
         MediaType type = MediaType.parseMediaType("application/json; charset=UTF-8");
         headers.setContentType(type);
@@ -46,17 +43,6 @@ public class AnkiServiceImpl implements AnkiService {
         return restTemplate.postForObject(ANKI_URL, formEntity, AnkiRespVo.class);
     }
 
-
-    @Override
-    public AnkiRespVo addNotes(List<NoteVo> noteVos) {
-        AnkiVo ankiVo = new AnkiVo();
-        ankiVo.setAction("addNotes");
-        Map<String, Object> params = new HashMap<String, Object>();
-        params.put("notes", noteVos);
-        ankiVo.setParams(params);
-        return post(ankiVo);
-    }
-
     @Override
     public AnkiRespVo addNote(Note note) {
         AnkiVo ankiVo = new AnkiVo();
@@ -64,7 +50,7 @@ public class AnkiServiceImpl implements AnkiService {
         Map<String, Object> params = Maps.newHashMap();
         params.put("note", note);
         ankiVo.setParams(params);
-        return post(ankiVo);
+        return postAnki(ankiVo);
     }
 
 }
