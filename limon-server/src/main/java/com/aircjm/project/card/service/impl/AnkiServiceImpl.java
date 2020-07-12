c11b65553ef129cb07cf9d0599ce1190e758ce5d
aircjm <aircjm@gmail.com>
2020-07-12 19:46:27 +0800

    生成对应的anki note

--- limon-server/src/main/java/com/aircjm/project/card/service/impl/AnkiServiceImpl.java
+++ limon-server/src/main/java/com/aircjm/project/card/service/impl/AnkiServiceImpl.java
@@ -1,14 +1,17 @@
 package com.aircjm.project.card.service.impl;
 
-import com.aircjm.project.anki.response.AnkiRespVo;
 import com.aircjm.project.anki.ankiconnect.AnkiVo;
 import com.aircjm.project.anki.ankiconnect.NoteVo;
+import com.aircjm.project.anki.response.AnkiRespVo;
 import com.aircjm.project.card.service.AnkiService;
 import com.aircjm.project.card.vo.anki.vo.Note;
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -33,10 +36,14 @@ public class AnkiServiceImpl implements AnkiService {
 
     @Override
     public AnkiRespVo post(AnkiVo ankiVo) {
+        HttpHeaders headers = new HttpHeaders();
+        MediaType type = MediaType.parseMediaType("application/json; charset=UTF-8");
+        headers.setContentType(type);
+        headers.add("Accept", MediaType.APPLICATION_JSON.toString());
         log.info("调用anki接口入参为:{}", JSON.toJSONString(ankiVo));
-        ResponseEntity<String> resp = restTemplate.postForEntity(ANKI_URL, ankiVo, String.class);
-        System.out.println(JSON.toJSONString(resp));
-        return JSON.parseObject(resp.getBody(), AnkiRespVo.class);
+        JSONObject jsonObj = JSONObject.parseObject(JSONObject.toJSONString(ankiVo));
+        HttpEntity<String> formEntity = new HttpEntity<>(jsonObj.toString(), headers);
+        return restTemplate.postForObject(ANKI_URL, formEntity, AnkiRespVo.class);
     }
 
 
