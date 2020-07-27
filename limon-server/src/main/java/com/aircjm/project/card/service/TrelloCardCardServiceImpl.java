3ce75b95e9f368d2d98309d86aeda32ce068f5c8
aircjm <aircjm@gmail.com>
2020-07-27 23:52:36 +0800

    feat(card): 卡片数据落地

--- limon-server/src/main/java/com/aircjm/project/card/service/TrelloCardCardServiceImpl.java
+++ limon-server/src/main/java/com/aircjm/project/card/service/TrelloCardCardServiceImpl.java
@@ -1,10 +1,13 @@
 package com.aircjm.project.card.service;
 
+import com.aircjm.project.card.domain.TrelloCard;
 import com.aircjm.project.card.domain.Webhook;
+import com.aircjm.project.card.mapper.TrelloCardMapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MyTrelloServiceImpl implements MyTrelloService {
+public class TrelloCardCardServiceImpl extends ServiceImpl<TrelloCardMapper, TrelloCard> implements TrelloCardService {
 
     @Override
     public Webhook createWebhook(String description, String callbackUrl, String idModel) {
