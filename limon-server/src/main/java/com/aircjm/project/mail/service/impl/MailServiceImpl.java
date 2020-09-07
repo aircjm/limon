d18631f7d1bdf5f1822d044a5c2924e248d06d29
aircjm <aircjm@gmail.com>
2020-09-07 20:07:03 +0800

    feat(邮箱): 添加邮箱

--- limon-server/src/main/java/com/aircjm/project/mail/service/impl/MailServiceImpl.java
+++ limon-server/src/main/java/com/aircjm/project/mail/service/impl/MailServiceImpl.java
@@ -1,14 +1,13 @@
 package com.aircjm.project.mail.service.impl;
 
+import cn.hutool.extra.mail.MailUtil;
 import com.aircjm.project.mail.service.MailService;
 import com.aircjm.project.mail.vo.SendMailRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import javax.mail.internet.MimeMessage;
 
 /**
  * 邮件service
@@ -25,17 +24,11 @@ public class MailServiceImpl implements MailService {
 
     @Override
     public void sendMail(SendMailRequest request) {
-        MimeMessage message = javaMailSender.createMimeMessage();
         try {
-            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(message, true);
-            mimeMessageHelper.setTo(request.getReceiver());
-            mimeMessageHelper.setFrom(request.getSender());
-            mimeMessageHelper.setSubject(request.getSubject());
-            mimeMessageHelper.setText(request.getHtml(), true);
+            MailUtil.send(request.getSender(), request.getSubject(), request.getHtml(), true, null);
         } catch (Exception e) {
             log.error("发送邮件异常");
             e.printStackTrace();
         }
-        javaMailSender.send(message);
     }
 }
