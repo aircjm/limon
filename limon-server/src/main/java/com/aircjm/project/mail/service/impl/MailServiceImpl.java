ffa8abd31cfa2c4f9183b0ed10133f2aaf6c453b
aircjm <aircjm@gmail.com>
2020-09-08 14:17:51 +0800

    feat(邮箱): 去除无用字段

--- limon-server/src/main/java/com/aircjm/project/mail/service/impl/MailServiceImpl.java
+++ limon-server/src/main/java/com/aircjm/project/mail/service/impl/MailServiceImpl.java
@@ -4,10 +4,9 @@ import cn.hutool.extra.mail.MailUtil;
 import com.aircjm.project.mail.service.MailService;
 import com.aircjm.project.mail.vo.SendMailRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
+import java.io.File;
 
 /**
  * 邮件service
@@ -18,14 +17,10 @@ import javax.annotation.Resource;
 @Service
 public class MailServiceImpl implements MailService {
 
-    @Resource
-    JavaMailSender javaMailSender;
-
-
     @Override
     public void sendMail(SendMailRequest request) {
         try {
-            MailUtil.send(request.getSender(), request.getSubject(), request.getHtml(), true, null);
+            MailUtil.send(request.getSender(), request.getSubject(), request.getHtml(), true, (File) null);
         } catch (Exception e) {
             log.error("发送邮件异常");
             e.printStackTrace();
