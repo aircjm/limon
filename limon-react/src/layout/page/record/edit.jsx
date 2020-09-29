082b70210a6daf1f41586061d15e58a4c66331b7
aircjm <aircjm@gmail.com>
2020-09-29 23:00:30 +0800

    feat(record):add

--- limon-react/src/layout/page/record/edit.jsx
+++ limon-react/src/layout/page/record/edit.jsx
@@ -9,14 +9,16 @@ function RecordEdit() {
         return {
             title: '',
             context: '',
-            status: 0
+            type: 0,
+            status: 0,
+            checked: true
         };
     }
 
     const [form, setForm] = React.useState(initFrom());
     const [option, setOption] = React.useState([]);
     const [loading, setLoading] = React.useState(false);
-    const [value, setValue] = React.useState('');
+    const [value, setValue] = React.useState(true);
 
 
     function submit() {
@@ -28,7 +30,8 @@ function RecordEdit() {
     return (
         <Form>
             <Form.Group widths='equal'>
-                <Form.Input fluid label='title' placeholder='title' value={form.title} onChange={(e) => setForm({ ...form, title: e.target.value })}/>
+                <Form.Input fluid label='title' placeholder='title' value={form.title}
+                            onChange={(e) => setForm({...form, title: e.target.value})}/>
                 <Form.Select
                     fluid
                     label='Gender'
@@ -36,29 +39,9 @@ function RecordEdit() {
                     placeholder='Gender'
                 />
             </Form.Group>
-            <Form.Group inline>
-                <label>Size</label>
-                <Form.Radio
-                    label='Small'
-                    value='0'
-                    checked={value === '0'}
-                    onClick={(e) => setForm({ ...form, status: e.target.value })}
-                />
-                <Form.Radio
-                    label='Medium'
-                    value='1'
-                    checked={value === '1'}
-                    onChange={(e) => setForm({ ...form, status: e.target.value })}
-                />
-                <Form.Radio
-                    label='Large'
-                    value='2'
-                    checked={value === '2'}
-                    onChange={(e) => setForm({ ...form, status: e.target.value })}
-                />
-            </Form.Group>
-            <Form.TextArea label='About' placeholder='Tell us more about you...' onChange={(e) => setForm({...form, context : e.target.value})} />
-            <Form.Checkbox label='I agree to the Terms and Conditions' />
+            <Form.TextArea label='About' placeholder='Tell us more about you...'
+                           onChange={(e) => setForm({...form, context: e.target.value})}/>
+            <Form.Checkbox label='I agree to the Terms and Conditions' defaultChecked={form.checked}  onClick={(e, data) => setForm({...form, checked : data.checked})}/>
             <Form.Button loading={loading} onClick={() => submit()}>Submit</Form.Button>
         </Form>
     )
