34181d41d5f71ff8d2c0db49807a8b92e0ce738e
aircjm <aircjm@gmail.com>
2020-10-07 22:07:59 +0800

    feat(react):登录

--- limon-react/src/bootstrap/pages/FixedMenuLayout.jsx
+++ limon-react/src/bootstrap/pages/FixedMenuLayout.jsx
@@ -1,94 +1,52 @@
 import React from 'react'
 import {Dropdown, Menu,} from 'semantic-ui-react'
+import {useHistory} from "react-router-dom";
 
-const FixedMenuLayout = () => (
-    <React.Fragment>
-        <Menu size='mini'>
-            <Menu.Item as='a' header>
-                {/*<Image size='mini' src='/logo.png' style={{ marginRight: '1em' }} />*/}
-                limon
-            </Menu.Item>
-            <Menu.Item as='a'>Home</Menu.Item>
-
-            <Dropdown item simple text='Dropdown'>
-                <Dropdown.Menu>
-                    <Dropdown.Item>List Item</Dropdown.Item>
-                    <Dropdown.Item>List Item</Dropdown.Item>
-                    <Dropdown.Divider/>
-                    <Dropdown.Header>Header Item</Dropdown.Header>
-                    <Dropdown.Item>
-                        <i className='dropdown icon'/>
-                        <span className='text'>Submenu</span>
-                        <Dropdown.Menu>
-                            <Dropdown.Item>List Item</Dropdown.Item>
-                            <Dropdown.Item>List Item</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown.Item>
-                    <Dropdown.Item>List Item</Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
+const FixedMenuLayout = () => {
 
 
-            <Menu.Item position='right'>Login</Menu.Item>
-        </Menu>
+    const history = useHistory();
 
-        {/*<Segment inverted vertical style={{ margin: '5em 0em 0em', padding: '5em 0em' }}>*/}
-        {/*<Container textAlign='center'>*/}
-        {/*<Grid divided inverted stackable>*/}
-        {/*<Grid.Column width={3}>*/}
-        {/*<Header inverted as='h4' content='Group 1' />*/}
-        {/*<List link inverted>*/}
-        {/*<List.Item as='a'>Link One</List.Item>*/}
-        {/*<List.Item as='a'>Link Two</List.Item>*/}
-        {/*<List.Item as='a'>Link Three</List.Item>*/}
-        {/*<List.Item as='a'>Link Four</List.Item>*/}
-        {/*</List>*/}
-        {/*</Grid.Column>*/}
-        {/*<Grid.Column width={3}>*/}
-        {/*<Header inverted as='h4' content='Group 2' />*/}
-        {/*<List link inverted>*/}
-        {/*<List.Item as='a'>Link One</List.Item>*/}
-        {/*<List.Item as='a'>Link Two</List.Item>*/}
-        {/*<List.Item as='a'>Link Three</List.Item>*/}
-        {/*<List.Item as='a'>Link Four</List.Item>*/}
-        {/*</List>*/}
-        {/*</Grid.Column>*/}
-        {/*<Grid.Column width={3}>*/}
-        {/*<Header inverted as='h4' content='Group 3' />*/}
-        {/*<List link inverted>*/}
-        {/*<List.Item as='a'>Link One</List.Item>*/}
-        {/*<List.Item as='a'>Link Two</List.Item>*/}
-        {/*<List.Item as='a'>Link Three</List.Item>*/}
-        {/*<List.Item as='a'>Link Four</List.Item>*/}
-        {/*</List>*/}
-        {/*</Grid.Column>*/}
-        {/*<Grid.Column width={7}>*/}
-        {/*<Header inverted as='h4' content='Footer Header' />*/}
-        {/*<p>*/}
-        {/*Extra space for a call to action inside the footer that could help re-engage users.*/}
-        {/*</p>*/}
-        {/*</Grid.Column>*/}
-        {/*</Grid>*/}
+    function toHome() {
+        history.push("/home")
+    }
 
-        {/*<Divider inverted section />*/}
-        {/*<Image centered size='mini' src='/logo.png' />*/}
-        {/*<List horizontal inverted divided link size='small'>*/}
-        {/*<List.Item as='a' href='#'>*/}
-        {/*Site Map*/}
-        {/*</List.Item>*/}
-        {/*<List.Item as='a' href='#'>*/}
-        {/*Contact Us*/}
-        {/*</List.Item>*/}
-        {/*<List.Item as='a' href='#'>*/}
-        {/*Terms and Conditions*/}
-        {/*</List.Item>*/}
-        {/*<List.Item as='a' href='#'>*/}
-        {/*Privacy Policy*/}
-        {/*</List.Item>*/}
-        {/*</List>*/}
-        {/*</Container>*/}
-        {/*</Segment>*/}
-    </React.Fragment>
-)
+    function toLogin() {
+        history.push("/login")
+    }
+
+    return (
+        <React.Fragment>
+            <Menu pointing secondary>
+                <Menu.Item as='a' header>
+                    {/*主页图标*/}
+                    {/* <Image size='mini' src='/logo.png' style={{ marginRight: '1em' }} />*/}
+                    limon
+                </Menu.Item>
+                <Menu.Item as='a' onClick={toHome}>Home</Menu.Item>
+
+                <Dropdown item simple text='Dropdown'>
+                    <Dropdown.Menu>
+                        <Dropdown.Item>List Item</Dropdown.Item>
+                        <Dropdown.Item>List Item</Dropdown.Item>
+                        <Dropdown.Divider/>
+                        <Dropdown.Header>Header Item</Dropdown.Header>
+                        <Dropdown.Item>
+                            <i className='dropdown icon'/>
+                            <span className='text'>Submenu</span>
+                            <Dropdown.Menu>
+                                <Dropdown.Item>List Item</Dropdown.Item>
+                                <Dropdown.Item>List Item</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown.Item>
+                        <Dropdown.Item>List Item</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+
+                <Menu.Item position='right' onClick={toLogin}>Log-in</Menu.Item>
+            </Menu>
+        </React.Fragment>
+    )
+}
 
 export default FixedMenuLayout
