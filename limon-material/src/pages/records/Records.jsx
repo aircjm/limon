46a867ad31c54303a3b6d72d36bd68de4061d998
aircjm <aircjm@gmail.com>
2020-10-19 11:10:21 +0800

    init material

---
+++ limon-material/src/pages/records/Records.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+// components
+import PageTitle from "../../components/PageTitle";
+import EditRecord from "./EditRecord";
+
+function Records() {
+
+    return (
+        <React.Fragment>
+            <PageTitle title="Records"/>
+            {/*<EditRecord/>*/}
+        </React.Fragment>
+    );
+}
+
+export default Records
