60ce0e6a9c0f5795f5e847beb1ea84f97d0a9229
aircjm <aircjm@gmail.com>
2020-11-03 17:06:57 +0800

    feat(eslint): eslint init

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,4 +1,20 @@
-import Vue from 'vue'
 import axios from 'axios'
+import { Notify } from 'quasar'
 
-Vue.prototype.$axios = axios
+const axiosInstance = axios.create({})
+
+axiosInstance.interceptors.response.use((response) => {
+  return response
+}, (error) => {
+  if (!error.response) {
+    Notify.create('errors.network')
+  }
+  return Promise.reject(error)
+})
+
+export default ({ Vue }) => {
+  Vue.prototype.$axios = axiosInstance
+  Vue.axios = Vue.prototype.$axios
+}
+
+export { axiosInstance }
