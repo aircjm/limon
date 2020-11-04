fc6d09cbd1ec355b2bcfafaef8eca0e23898844c
aircjm <aircjm@gmail.com>
2020-11-04 13:59:46 +0800

    feat(dev): proxy devServer

--- quasar.conf.js
+++ quasar.conf.js
@@ -77,7 +77,17 @@ module.exports = function (/* ctx */) {
     devServer: {
       https: false,
       port: 8080,
-      open: true // opens browser window automatically
+      open: true, // opens browser window automatically
+      proxy: {
+        // proxy all requests starting with /api to jsonplaceholder
+        '/api': {
+          target: 'http://localhost:8087',
+          changeOrigin: true,
+          pathRewrite: {
+            '^/api': '/api'
+          }
+        }
+      }
     },
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
