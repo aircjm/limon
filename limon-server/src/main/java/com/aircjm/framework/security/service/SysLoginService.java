f34bb82c925d87110176cdb21003b90836b218bd
aircjm <aircjm@gmail.com>
2020-11-21 15:59:27 +0800

    feat(i8n): 错误验证码修改

--- limon-server/src/main/java/com/aircjm/framework/security/service/SysLoginService.java
+++ limon-server/src/main/java/com/aircjm/framework/security/service/SysLoginService.java
@@ -1,12 +1,5 @@
 package com.aircjm.framework.security.service;
 
-import javax.annotation.Resource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Component;
 import com.aircjm.common.constant.Constants;
 import com.aircjm.common.exception.CustomException;
 import com.aircjm.common.exception.user.CaptchaException;
@@ -17,6 +10,14 @@ import com.aircjm.framework.manager.AsyncManager;
 import com.aircjm.framework.manager.factory.AsyncFactory;
 import com.aircjm.framework.redis.RedisCache;
 import com.aircjm.framework.security.LoginUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 /**
  * 登录校验方法
@@ -37,11 +38,11 @@ public class SysLoginService
 
     /**
      * 登录验证
-     * 
+     *
      * @param username 用户名
      * @param password 密码
-     * @param captcha 验证码
-     * @param uuid 唯一标识
+     * @param code     验证码
+     * @param uuid     唯一标识
      * @return 结果
      */
     public String login(String username, String password, String code, String uuid)
@@ -51,12 +52,12 @@ public class SysLoginService
         redisCache.deleteObject(verifyKey);
         if (captcha == null)
         {
-            AsyncManager.me().execute(AsyncFactory.recordLogininfor(username, Constants.LOGIN_FAIL, MessageUtils.message("user.jcaptcha.expire")));
+            AsyncManager.me().execute(AsyncFactory.recordLogininfor(username, Constants.LOGIN_FAIL, MessageUtils.message("user.captcha.expire")));
             throw new CaptchaExpireException();
         }
         if (!code.equalsIgnoreCase(captcha))
         {
-            AsyncManager.me().execute(AsyncFactory.recordLogininfor(username, Constants.LOGIN_FAIL, MessageUtils.message("user.jcaptcha.error")));
+            AsyncManager.me().execute(AsyncFactory.recordLogininfor(username, Constants.LOGIN_FAIL, MessageUtils.message("user.captcha.error")));
             throw new CaptchaException();
         }
         // 用户验证
