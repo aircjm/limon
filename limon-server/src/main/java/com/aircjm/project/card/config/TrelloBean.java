9d5ecf1154a1d32f506c1a2253bacde51eb35a80
aircjm <aircjm@gmail.com>
2020-12-07 17:24:02 +0800

    feat(trello): 配置更新

--- limon-server/src/main/java/com/aircjm/project/card/config/TrelloBean.java
+++ limon-server/src/main/java/com/aircjm/project/card/config/TrelloBean.java
@@ -1,18 +1,17 @@
 package com.aircjm.project.card.config;
 
-import com.aircjm.project.system.domain.SysConfig;
-import com.aircjm.project.system.mapper.SysConfigMapper;
+import cn.hutool.core.lang.TypeReference;
+import cn.hutool.json.JSONUtil;
+import com.aircjm.project.system.service.ISysConfigService;
 import com.julienvey.trello.Trello;
 import com.julienvey.trello.impl.TrelloImpl;
 import com.julienvey.trello.impl.http.ApacheHttpClient;
-import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.Resource;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * Trello配置
@@ -23,22 +22,18 @@ import java.util.stream.Collectors;
 public class TrelloBean {
 
     @Resource
-    private SysConfigMapper sysConfigMapper;
-
+    private ISysConfigService sysConfigService;
 
     @Bean
     public Trello getTrelloApi() {
-        SysConfig sysConfig = new SysConfig();
-        sysConfig.setConfigKey("trello");
-        List<SysConfig> trelloConfig = sysConfigMapper.selectConfigList(sysConfig);
-        if (CollectionUtils.isNotEmpty(trelloConfig)) {
-            Map<String, String> map = trelloConfig.stream().collect(Collectors.toMap(SysConfig::getConfigKey, SysConfig::getConfigValue));
-            return new TrelloImpl(map.get("trello.api.key"), map.get("trello.token"), new ApacheHttpClient());
+        String trelloConfig = sysConfigService.selectConfigByKey("trello.config");
+        if (StringUtils.isNotBlank(trelloConfig)) {
+            Map<String, String> trelloMap = JSONUtil.toBean(trelloConfig, new TypeReference<Map<String, String>>() {
+            }, true);
+            return new TrelloImpl(trelloMap.get("trello.api.key"), trelloMap.get("trello.token"), new ApacheHttpClient());
         } else {
             return new TrelloImpl(null, null, new ApacheHttpClient());
         }
 
     }
-
-
 }
