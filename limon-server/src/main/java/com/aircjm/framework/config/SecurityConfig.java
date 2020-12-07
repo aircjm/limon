eeedbd51cf8a25315f44e5eac27939afaad7364e
aircjm <aircjm@gmail.com>
2020-12-07 19:56:54 +0800

    feat(email): 消息通知处理

--- limon-server/src/main/java/com/aircjm/framework/config/SecurityConfig.java
+++ limon-server/src/main/java/com/aircjm/framework/config/SecurityConfig.java
@@ -105,7 +105,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
                 .antMatchers("/webjars/**").anonymous()
                 .antMatchers("/*/api-docs").anonymous()
                 .antMatchers("/druid/**").anonymous()
-                .antMatchers("/api/record/noticeAdd").anonymous()
+                .antMatchers("/api/task/notice").anonymous()
                 // 除上面外的所有请求全部需要鉴权认证
                 .anyRequest().authenticated()
                 .and()
--- limon-server/src/main/java/com/aircjm/framework/message/EmailServiceImpl.java
+++ limon-server/src/main/java/com/aircjm/framework/message/EmailServiceImpl.java
@@ -2,10 +2,12 @@ package com.aircjm.framework.message;
 
 import cn.hutool.extra.mail.MailAccount;
 import cn.hutool.extra.mail.MailUtil;
+import cn.hutool.json.JSONUtil;
 import com.aircjm.common.exception.CustomException;
 import com.aircjm.common.utils.StringUtils;
 import com.aircjm.project.system.domain.SysUser;
 import com.aircjm.project.system.mapper.SysUserMapper;
+import com.aircjm.project.system.service.ISysConfigService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -21,7 +23,12 @@ public class EmailServiceImpl implements MessageService {
     private SysUserMapper sysUserMapper;
 
     @Resource
-    private MailAccount mailAccount;
+    private ISysConfigService sysConfigService;
+
+    public MailAccount getMailAccount() {
+        String mailConfig = sysConfigService.selectConfigByKey("mail.config");
+        return JSONUtil.toBean(mailConfig, MailAccount.class, true);
+    }
 
 
     @Override
@@ -30,7 +37,7 @@ public class EmailServiceImpl implements MessageService {
         if (Objects.isNull(sysUser) || StringUtils.isEmpty(sysUser.getEmail())) {
             throw new CustomException("当前用户不存在");
         }
-        MailUtil.send(mailAccount, sysUser.getEmail(), title, context, false);
+        MailUtil.send(getMailAccount(), sysUser.getEmail(), title, context, false);
     }
 
 }
