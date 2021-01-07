ef3cdf5577ddbdccb7b9a73094caa0e8b7e9a331
aircjm <aircjm@gmail.com>
2021-01-07 15:43:50 +0800

    feat(task):  优化 删除无用代码

--- src/pages/task/AllTask.vue
+++ src/pages/task/AllTask.vue
@@ -188,12 +188,6 @@ export default {
       title: '',
       recordType: null,
       date: null,
-      options: [
-        {
-          id: 1,
-          desc: 'first'
-        }
-      ],
       setTimeForm: {
         loading: false,
         task: {}
@@ -212,109 +206,6 @@ export default {
       },
       filter: '',
       loading: false,
-      pagination: {
-        sortBy: 'desc',
-        descending: false,
-        page: 1,
-        rowsPerPage: 10,
-        rowsNumber: 10
-      },
-      selected: [],
-      columns: [
-        {
-          // 唯一的ID
-          // 标识列
-          // （由pagination.sortBy使用，“body-cell-[name]”插槽，...）
-          name: 'id',
-
-          // 头部标签
-          label: 'id',
-
-          // 行对象属性以确定此列的值
-          field: 'id',
-          // 或者field: row => row.some.nested.prop
-
-          // （可选）如果我们使用可见列，这个列将始终可见
-          required: true,
-
-          // （可选）对齐
-          align: 'left',
-
-          // （可选）告诉QTable你想要这个列可排序
-          // sortable: true,
-
-          // （可选）比较功能，如果你有
-          //  一些自定义数据或想要一个特定的方式来比较两行
-          sort: (a, b, rowA, rowB) => parseInt(a, 10) - parseInt(b, 10),
-          // 函数返回值：
-          // *小于0，然后将a排序为低于b的索引，即首先出现
-          // *为0，则相对于彼此保持a和b不变，但相对于所有不同的元素进行排序
-          // *大于0，则将b排序为低于a的索引，即b先到达
-
-          // （可选）您可以使用函数格式化数据
-          // format: (val, row) => `${val}%`,
-          // 另一个格式示例：
-          // format: val => `${val}`,
-          //   ? /* 选中Unicode复选标记 */ "\u2611"
-          //   : /* 未选中Unicode复选标记 */ "\u2610",
-
-          // body td:
-          style: 'width: 20px'
-          // classes: 'my-special-class',
-
-          // (v1.3.0+) header th:
-          // headerStyle: 'width: 500px',
-          // headerClasses: 'my-special-class'
-        },
-        {
-          name: 'title',
-          label: 'title',
-          field: 'title',
-          align: 'left',
-          style: 'width:200px'
-        },
-        {
-          name: 'context',
-          label: 'context',
-          field: 'context',
-          align: 'left'
-        },
-        {
-          name: 'status',
-          label: '状态',
-          field: 'status',
-          align: 'left',
-          style: 'width: 20px'
-        },
-        {
-          name: 'dueTime',
-          label: '截止时间（通知）',
-          field: 'dueTime',
-          align: 'left',
-          style: 'width:100px'
-        },
-        {
-          name: 'startTime',
-          label: '开始时间',
-          field: 'startTime',
-          align: 'left',
-          style: 'width:100px'
-        },
-        {
-          name: 'endTime',
-          label: '结束时间',
-          field: 'endTime',
-          align: 'left',
-          style: 'width:100px'
-        },
-        {
-          name: 'options',
-          label: '操作',
-          field: 'options',
-          align: 'center',
-          style: 'width: 100px'
-        }
-      ],
       tasks: []
     }
   },
