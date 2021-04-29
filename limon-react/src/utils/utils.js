d8f628467b61cabb286d15af718c07e515d36242
aircjm <aircjm@gmail.com>
2021-04-29 14:41:17 +0800

    feat(react): util add

--- limon-react/src/utils/utils.js
+++ limon-react/src/utils/utils.js
@@ -4,3 +4,9 @@ export const IsNumber = s => /^\d+$/.test(s);
 export function getToken() {
     return localStorage.getItem("token")
 }
+
+export function setToken(token) {
+    if (token) {
+        localStorage.setItem("token", token)
+    }
+}
