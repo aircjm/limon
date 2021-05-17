fa94eb03771c0861f445d5ddcad0c11e9822b987
aircjm <aircjm@gmail.com>
2021-05-17 10:52:23 +0800

    feat(vite): add vite

---
+++ vite-react/vite.config.js
@@ -0,0 +1,24 @@
+import {defineConfig} from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [reactRefresh()],
+  server: {
+    port: 3050,
+    proxy: {
+      // 选项写法
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+      // 正则表达式写法
+      '^/fallback/.*': {
+        target: 'http://jsonplaceholder.typicode.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/fallback/, '')
+      }
+    }
+  }
+})
