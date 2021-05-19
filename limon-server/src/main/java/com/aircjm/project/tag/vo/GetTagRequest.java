50c07287e78c6be8f34e111468151b0d75b9210f
aircjm <aircjm@gmail.com>
2021-05-19 23:31:48 +0800

    feat(tag):  添加注释

--- limon-server/src/main/java/com/aircjm/project/tag/vo/GetTagRequest.java
+++ limon-server/src/main/java/com/aircjm/project/tag/vo/GetTagRequest.java
@@ -2,10 +2,22 @@ package com.aircjm.project.tag.vo;
 
 import com.aircjm.project.tag.domain.Tag;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import lombok.Data;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+
+/**
+ * @author aircjm
+ */
+@ApiModel(description = "查询标签列表数据请求参数")
+@Setter
+@Getter
+@EqualsAndHashCode(callSuper = true)
 public class GetTagRequest extends Page<Tag> {
 
+    @ApiModelProperty(value = "标签名称 支持模糊搜索")
     private String name;
 }
