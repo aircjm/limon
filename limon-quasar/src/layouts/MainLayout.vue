0e245c175eede03da9761e7bba5aa9db60a3299f
aircjm <aircjm@gmail.com>
2021-07-09 20:36:34 +0800

    feat(优化):  优化标题

--- limon-quasar/src/layouts/MainLayout.vue
+++ limon-quasar/src/layouts/MainLayout.vue
@@ -1,28 +1,71 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar>
-        <q-btn stretch flat icon="menu" aria-label="Menu"
+      <q-toolbar class="bg-white-4 shadow-2">
+        <q-btn flat round icon="menu"
                @click="leftDrawerOpen = !leftDrawerOpen"/>
-        <q-separator dark vertical/>
-        <q-btn stretch flat icon="home"  to="/" aria-label="Dash App"/>
-        <q-btn stretch flat label="AllTask" to="/allTask"/>
-        <q-btn stretch flat label="TAG" to="/tag"/>
-        <q-btn stretch flat label="Timer" to="/timer"/>
+        <q-btn flat to="/" aria-label="Dash App"/>
+        <div v-if="$q.screen.gt.xs">
+          <q-btn flat label="AllTask" to="/allTask"/>
+          <q-btn flat label="TAG" to="/tag"/>
+          <q-btn flat label="Timer" to="/timer"/>
+        </div>
+
         <q-space/>
-        <q-btn-dropdown flat dense round icon="help">
-          <q-list>
+        <q-btn-dropdown v-if="$q.screen.gt.xs" icon="help" flat>
+          <q-list  style="min-width: 100px">
             <q-item clickable>
-              <q-item-section>
-                <q-item-label @click="jumpLink('https://next.quasar.dev/')">
-                  quasar v2 Docs
-                </q-item-label>
+              <q-item-section @click="jumpLink('https://next.quasar.dev/')">
+                quasar v2 Docs
               </q-item-section>
             </q-item>
           </q-list>
         </q-btn-dropdown>
-        <q-separator dark vertical/>
-        <q-btn flat dense round icon="login"/>
+        <q-separator vertical inset />
+        <q-btn v-if="login" stretch flat no-wrap>
+          <q-avatar rounded size="20px">
+            <img src="loginImg">
+          </q-avatar>
+          <q-icon name="arrow_drop_down" size="16px"/>
+          <q-menu auto-close>
+            <q-list>
+              <q-item>
+                <q-item-section>
+                  <div>Signed in as <strong>Mary</strong></div>
+                </q-item-section>
+              </q-item>
+              <q-separator separator inset/>
+              <q-item clickable>
+                <q-item-section>
+                  <div>
+                    <q-icon name="tag_faces" color="blue-9" size="18px"/>
+                    Set your status
+                  </div>
+                </q-item-section>
+              </q-item>
+              <q-separator/>
+              <q-item clickable>
+                <q-item-section>Your profile</q-item-section>
+              </q-item>
+              <q-separator/>
+              <q-item clickable>
+                <q-item-section>Help</q-item-section>
+              </q-item>
+              <q-item clickable>
+                <q-item-section>Settings</q-item-section>
+              </q-item>
+              <q-item clickable>
+                <q-item-section>
+                  <div>
+                    <q-icon name="logout" color="blue-9" size="18px"/>
+                    Sign out
+                  </div>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-menu>
+        </q-btn>
+        <q-btn flat v-else icon="login"/>
       </q-toolbar>
     </q-header>
 
@@ -34,16 +77,19 @@
 
 <script>
 
-import {reactive, toRefs} from "@vue/reactivity";
+import {reactive, ref, toRefs} from "@vue/reactivity";
 
 
 export default {
   name: 'MainLayout',
-  setup() {
+  setup(props, context) {
     const state = reactive({
       leftDrawerOpen: true,
-      miniState: true
+      miniState: true,
+      loginImg: ''
     })
+
+    const login = ref(false)
 
     const jumpLink = (url) => {
       window.open(url)
@@ -51,7 +97,7 @@ export default {
 
     return {
       ...toRefs(state),
-      jumpLink
+      jumpLink, login
     }
 
   }
