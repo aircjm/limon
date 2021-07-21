cffdb433c99394c80e8c6546f06e0ad0f466efea
aircjm <aircjm@gmail.com>
2021-07-21 10:48:02 +0800

    feat(react): 移除react

--- limon-react/src/components/hooks/ValicatePath.jsx
+++
@@ -1,9 +0,0 @@
-import {useLocation} from "react-router-dom";
-
-const ValidatePath = (path) => {
-  const location = useLocation();
-  const {pathname} = location;
-  return pathname === path;
-}
-
-export default ValidatePath;
