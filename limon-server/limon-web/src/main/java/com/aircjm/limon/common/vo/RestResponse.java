5584cc5ed977b110481ea55b018d42098bc61fd0
aircjm <aircjm@gmail.com>
2021-08-29 10:16:27 +0800

    feat(common): response 修改

--- limon-server/limon-web/src/main/java/com/aircjm/limon/common/vo/RestResponse.java
+++ limon-server/limon-web/src/main/java/com/aircjm/limon/common/vo/RestResponse.java
@@ -23,19 +23,19 @@ public class RestResponse<T> {
     private RestResponse() {
     }
 
-    public static RestResponse successEmpty() {
+    public static <T> RestResponse successEmpty() {
         return new RestResponse<>(200, "success", null);
     }
 
 
 
-    public static RestResponse successData(Object data) {
+    public static <T> RestResponse successData(T data) {
         return new RestResponse<>(200, "success", data);
     }
 
 
 
-    public static RestResponse success(int code, String msg, Object data) {
+    public static <T> RestResponse success(int code, String msg, T data) {
         return new RestResponse<>(code, msg, data);
     }
 
