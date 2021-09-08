823d90586deaf522a9c84f2d4933e90472da157f
aircjm <aircjm@gmail.com>
2021-09-08 16:09:07 +0800

    feat(init): init project

---
+++ limon-server/src/main/java/com/aircjm/limon/LimonServerApplication.java
@@ -0,0 +1,13 @@
+package com.aircjm.limon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimonServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimonServerApplication.class, args);
+    }
+
+}
