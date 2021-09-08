210dd293f3763d02c7cb28562ee4a71b34b17a78
aircjm <aircjm@gmail.com>
2021-09-08 16:09:07 +0800

    feat(init): init project

---
+++ limon-server/src/test/java/com/aircjm/limon/LimonServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aircjm.limon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimonServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
