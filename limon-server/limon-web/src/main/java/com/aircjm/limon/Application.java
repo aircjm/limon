5be15f963eb0a68a92b2651d78867aebd2b053cd
aircjm <aircjm@gmail.com>
2021-10-04 17:22:41 +0800

    feat(common): 优化提醒

--- limon-server/limon-web/src/main/java/com/aircjm/limon/Application.java
+++ limon-server/limon-web/src/main/java/com/aircjm/limon/Application.java
@@ -14,8 +14,8 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        System.out.println("////////////////////////////////////////////////////////////////////\n" +
-                "//                         项目启动 success                        //\n" +
-                "///////////////////////////////////////////////////////////////////");
+        System.out.println("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n" +
+                "||                         项目启动 success                       ||\n" +
+                "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
     }
 }
