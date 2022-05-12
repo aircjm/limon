d46b6aa01f6148927242953f56e2c2fbf9fa07d9
aircjm <aircjm@gmail.com>
2022-05-12 14:44:13 +0800

    feat(init): 添加注释

--- examples/01helloworld/test01_helloworld.rs
+++ examples/01helloworld/test01_helloworld.rs
@@ -3,10 +3,11 @@ fn main() {
     test_push_str();
     create_phone_number(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
 
-int_convert_str(10);
-
+    int_convert_str(10);
 }
 
+/// 字符串拼接
+/// 两个字符串合并成一个
 fn test_push_str() {
     let mut company = "hello".to_string();
     company.push_str("world");
@@ -14,10 +15,10 @@ fn test_push_str() {
 }
 
 fn create_phone_number(numbers: &[u8]) -> String {
-    return "()".to_string();
+    return "(" + ")";
 }
 
-
-fn int_convert_str(i : i32) -> String {
+/// 转化对应数字为字符串
+fn int_convert_str(i: i32) -> String {
     return "String".to_string();
 }
