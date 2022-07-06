b5d8f259bae16290f143fd6d36a9828880185c16
aircjm <aircjm@gmail.com>
2022-07-06 16:36:35 +0800

    feat(project): vite support

--- limon-quasar/src/App.vue
+++ limon-quasar/src/App.vue
@@ -1,10 +1,20 @@
 <template>
-  <router-view/>
+  <AppProvider>
+    <router-view v-slot="{ Component }">
+      <component :is="Component" />
+    </router-view>
+  </AppProvider>
 </template>
-<script>
-import {defineComponent} from 'vue';
 
-export default defineComponent({
-  name: 'App'
-})
+<script setup>
+import AppProvider from '@/components/common/AppProvider.vue'
 </script>
+
+<style lang="scss">
+#app {
+  height: 100%;
+  .n-config-provider {
+    height: inherit;
+  }
+}
+</style>
