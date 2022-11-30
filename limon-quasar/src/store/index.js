b8ea6a9ae0ec08e3372fe7948418bff2bf740fc8
aircjm <aircjm@gmail.com>
2022-11-30 09:42:00 +0800

    feat(init): init

--- limon-quasar/src/store/index.js
+++
@@ -1,5 +0,0 @@
-import { createPinia } from 'pinia'
-
-export function setupStore(app) {
-  app.use(createPinia())
-}
--- limon-quasar/src/utils/auth.js
+++
@@ -1,8 +0,0 @@
-import { router } from '@/router'
-
-export function toLogin() {
-  router.replace({
-    path: '/login',
-    query: { ...router.currentRoute.query, redirect: router.currentRoute.path },
-  })
-}
