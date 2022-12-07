e3a6a4f047da55ada853866b4f17774a68c6752a
aircjm <aircjm@gmail.com>
2022-12-07 19:21:55 +0800

    config add

--- lib-study/study02-actix-web/src/main.rs
+++ lib-study/study02-actix-web/src/main.rs
@@ -1,9 +1,10 @@
-use std::{path::Path, collections::HashMap};
-use serde::{Deserialize, Serialize};
 use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 use config::{Config, File};
+use serde::{Deserialize, Serialize};
 use std::fs;
 use std::io::prelude::*;
+use std::path::Path;
+use std::process::exit;
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -15,53 +16,46 @@ async fn hello(req: HttpRequest) -> impl Responder {
     format!("Hello {}!", &name)
 }
 
-
-#[derive(Deserialize, Serialize,Debug)]
+#[derive(Deserialize, Serialize, Debug)]
 pub struct Cfg {
-    setting : String,
+    setting: String,
     git: i32,
-    web: WebConfig
+    web: WebConfig,
 }
 
-
-#[derive(Deserialize, Serialize,Debug)]
+#[derive(Deserialize, Serialize, Debug)]
 pub struct WebConfig {
-    port: i32
+    port: i32,
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let settings_result = Config::builder()
+        .add_source(File::from(Path::new("data/config.json")))
+        .build();
+    let settings = match settings_result {
+        Ok(config) => config,
+        Err(_) => {
+            fs::create_dir_all("data")?;
+            let mut buffer = std::fs::File::create("data/config.json")?;
+            buffer.flush()?;
+            println!("require config file");
+            exit(1);
+        }
+    };
 
-
-
-    let settings_result = Config::builder().add_source(File::from(Path::new("conf/config.json")))
-    .build();
-   let settings = match settings_result {
-    Ok(config) => config,
-    Err(e) => {
-        fs::create_dir_all("conf")?;
-        let mut buffer = std::fs::File::create("conf/config.json")?;
-        buffer.flush()?;
-        !todo!("need new write config file");
-        panic!("require config file");
-    },
-};
-
-        // Print out our settings (as a HashMap)
-        println!(
-            "\n{:?} \n\n-----------",
-            settings
-                .try_deserialize::<Cfg>()
-                .unwrap()
-        );
-
+    // Print out our settings (as a HashMap)
+    println!(
+        "\n{:?} \n\n-----------",
+        settings.try_deserialize::<Cfg>().unwrap()
+    );
 
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(hello))
             .route("/{name}", web::get().to(greet))
     })
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
