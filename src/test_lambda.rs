4834cd009d13d07c96131eae9bf5be9e04d037fa
aircjm <aircjm@gmail.com>
2023-06-08 17:30:00 +0800

    feat(lambda): add

--- src/test_lambda.rs
+++ src/test_lambda.rs
@@ -17,3 +17,38 @@ pub fn test_simple() {
     })
 
 }
+
+
+
+
+#[test]
+pub fn test_lambda_fn() {
+
+    let fn_i32 = |x:i32| println!("{}",x);
+    fn_i32(12);
+
+    let vec_i32 = vec![1,2,3,4,5,6];
+    vec_i32.into_iter().for_each(fn_i32);
+
+}
+
+#[test]
+pub fn test_lambda_filter() {
+
+
+    let vec_i32 = vec![1,2,3,4,5,6,7,8,9,10,11,12];
+    let i = vec_i32.into_iter()
+        .filter(|item| *item < 6)
+        .inspect(|f| println!("{}", *f))
+        .collect::<Vec<i32>>();
+
+    // dbg!("{:?}", i);
+
+    println!("{:?}", i);
+    println!("{:?}", i);
+
+}
+
+
+
+
