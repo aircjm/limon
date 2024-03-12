d96a0a8f36bc414e7129a1024adcd55443b65da7
aircjm <aircjm@gmail.com>
2024-03-12 10:32:58 +0800

    study rust

--- src/test_str.rs
+++ src/test_str.rs
@@ -1,3 +1,55 @@
+
+
+#[test]
+// Function to demonstrate different ways of creating and manipulating Strings
+pub fn test_string_guide() {
+
+    // Create a String from a &str using the to_string() method
+    let to_string = "Learn Rust".to_string();
+    println!("get String from &str.to_string() method ,result is {to_string}");
+
+    // Replace a substring in the String using the replace() method
+    let replace_string = to_string.replace("Learn", "Study");
+    println!("String.replace() result is {replace_string}");
+
+    // Create a String from a &str using the to_owned() method
+    let owner_string = "Learn Rust String".to_owned();
+    println!("get String from &str.to_owned() method ,result is {owner_string}");
+
+    // Create a String using the String::from() method
+    let from_string = String::from("Learn Rust from method");
+    println!("get String from String::from() method ,result is {from_string}");
+}
+
+#[test]
+// Function to demonstrate passing &str and String as parameters
+pub fn test_str_param() {
+
+    // Define a &str
+    let str = "learn rust";
+    print(str);
+    // mismatched types expected reference `&std::string::String` found reference `&str`
+    // print_String(str);
+
+    // Create a String
+    let string = "Learn Rust String".to_string();
+
+    // Call the print function with &str parameter
+    print(&string);
+    // Call the print_string function with String parameter
+    print_string(&string);
+}
+
+// Function to print a &str
+pub fn print(str: &str) {
+    println!("str result is {}", str);
+}
+
+// Function to print a String
+pub fn print_string(str: &String) {
+    println!("str result is {}", str);
+}
+
 #[test]
 pub fn test_lowercase() {
     // lowercase
