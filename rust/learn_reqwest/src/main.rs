0feb246634b91fcce80006875cbb208a3aadc0f6
aircjm <aircjm@gmail.com>
2024-05-21 15:09:42 +0800

    feat(reqwest): test get fun

--- rust/learn_reqwest/src/main.rs
+++ rust/learn_reqwest/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    println!("hello world!");
+}
+
+// #[tokio::main]
+// async fn main() -> Result<(), ()> {
+//     println!("hello world!");
+//     Ok(())
+// }
+
+
+#[tokio::test]
+// need return a result, because the ? operator can only be used in an async block that returns
+// Result or Option.
+async fn test_simple() -> Result<(), reqwest::Error> {
+    let text = reqwest::get("https://www.rust-lang.org").await?.text().await?;
+
+    println!("text is {}", text);
+
+    Ok(())
 }
