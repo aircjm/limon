177c017f3883751be05f18556b3830b6589c4570
aircjm <aircjm@gmail.com>
2024-06-20 17:58:08 +0800

    feat(project): monorepo add

--- rust/learn_reqwest/src/main.rs
+++ rust/learn_reqwest/src/main.rs
@@ -20,3 +20,16 @@ async fn test_simple() -> Result<(), reqwest::Error> {
 
     Ok(())
 }
+
+
+
+#[tokio::test]
+// need return a result, because the ? operator can only be used in an async block that returns
+// Result or Option.
+async fn test_json() -> Result<(), reqwest::Error> {
+    // let text = reqwest::get("https://www.rust-lang.org").await?.json().await?;
+
+    // println!("text is {}", text);
+
+    Ok(())
+}
