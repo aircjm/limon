0600f37579814fc4341f3dec88dac0a66cf075bc
aircjm <aircjm@gmail.com>
2024-08-30 16:46:11 +0800

    init bing in vue

--- vue/vite-naiveui/src/App.vue
+++ vue/vite-naiveui/src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import Reactivity from "./components/Reactivity.vue";
 </script>
 
 <template>
@@ -12,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  <Reactivity />
 </template>
 
 <style scoped>
---
+++ vue/vite-naiveui/src/components/Reactivity.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import {computed, ref} from 'vue'
+import {NDatePicker} from 'naive-ui';
+
+function changeDate(value: number, formatValue: string) {
+  console.log("value is "+ value);
+  console.log("formatValue is "+ formatValue);
+}
+
+
+const is_all_day = ref<boolean>(false);
+const dateType = computed(() => {
+  if (is_all_day.value) {
+    return "date";
+  } else {
+    return "datetime";
+  }
+})
+
+</script>
+
+<template>
+
+
+  <n-switch v-model:value="is_all_day" />
+  {{is_all_day}}
+  {{dateType}}
+  <div class="card">
+    <NDatePicker :type="dateType" :on-update:value="changeDate" />
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
