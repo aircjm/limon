8a15d49454473c299e33d89ee707705931085b6e
aircjm <aircjm@gmail.com>
2024-09-02 16:28:49 +0800

    feat(project): add router and n-flex

--- vue/vite-naiveui/src/pages/Home.vue
+++ vue/vite-naiveui/src/pages/Home.vue
@@ -3,14 +3,8 @@
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <p>Home</p>
   </div>
-  <Reactivity />
 </template>
 
 <style scoped>
---
+++ vue/vite-naiveui/src/pages/LayoutTest.vue
@@ -0,0 +1,8 @@
+<template>
+  <n-layout embedded content-style="padding: 24px;">
+    <n-card>
+      只要是 看到天边云一朵<br>
+      逐天拢有好心情
+    </n-card>
+  </n-layout>
+</template>
