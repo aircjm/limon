6e08b5203c0de11aed28a0c3a52b8f48ef043db1
aircjm <aircjm@gmail.com>
2024-09-04 11:35:43 +0800

    feat(project): add n-grid

---
+++ vue/vite-naiveui/src/pages/CssFlex.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>Css Flex Use</p>
+  </div>
+  <n-grid x-gap="12" :cols="4">
+    <n-gi>
+      <div class="light-green" />
+    </n-gi>
+    <n-gi>
+      <div class="green" />
+    </n-gi>
+    <n-gi>
+      <div class="light-green" />
+    </n-gi>
+    <n-gi>
+      <div class="green" />
+    </n-gi>
+  </n-grid>
+
+</template>
+
+<style scoped>
+.light-green {
+  height: 108px;
+  background-color: rgba(0, 128, 0, 0.12);
+}
+.green {
+  height: 108px;
+  background-color: rgba(0, 128, 0, 0.24);
+}
+</style>
