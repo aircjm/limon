edc895c0fdc7d51ef10fa6ab9f196601d13e7f6a
aircjm <aircjm@gmail.com>
2024-09-04 11:37:17 +0800

    feat(project): add n-grid into n-card

--- vue/vite-naiveui/src/pages/CssFlex.vue
+++ vue/vite-naiveui/src/pages/CssFlex.vue
@@ -2,20 +2,36 @@
   <div>
     <p>Css Flex Use</p>
   </div>
-  <n-grid x-gap="12" :cols="4">
-    <n-gi>
-      <div class="light-green" />
-    </n-gi>
-    <n-gi>
-      <div class="green" />
-    </n-gi>
-    <n-gi>
-      <div class="light-green" />
-    </n-gi>
-    <n-gi>
-      <div class="green" />
-    </n-gi>
-  </n-grid>
+
+
+  <n-card>
+    <n-grid x-gap="12" :cols="4">
+      <n-gi>
+        <div class="light-green" />
+      </n-gi>
+      <n-gi>
+        <div class="green" />
+      </n-gi>
+      <n-gi>
+        <div class="light-green" />
+      </n-gi>
+      <n-gi>
+        <div class="green" />
+      </n-gi>
+    </n-grid>
+  </n-card>
+
+  <n-card>
+    <n-grid x-gap="12" :cols="2">
+      <n-gi>
+        <div class="light-green" />
+      </n-gi>
+      <n-gi>
+        <div class="green" />
+      </n-gi>
+    </n-grid>
+  </n-card>
+
 
 </template>
 
@@ -29,3 +45,5 @@
   background-color: rgba(0, 128, 0, 0.24);
 }
 </style>
+<script setup lang="ts">
+</script>
