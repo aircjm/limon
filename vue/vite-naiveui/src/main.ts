c542d1c54c1e1d1eb6c714e171b01d20b00cf80c
aircjm <aircjm@gmail.com>
2024-09-10 16:50:42 +0800

    feat(project): add tailwind css

--- vue/vite-naiveui/src/main.ts
+++ vue/vite-naiveui/src/main.ts
@@ -1,8 +1,14 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
 import {router} from "./routes/route.ts";
 
-createApp(App)
-    .use(router)
+const app = createApp(App);
+
+// const meta = document.createElement('meta')
+// meta.name = 'naive-ui-style'
+// document.head.appendChild(meta)
+
+
+app.use(router)
     .mount('#app')
