9f6f8481eb459fc34217b2ef3d0847de92468162
aircjm <aircjm@gmail.com>
2024-09-10 17:26:56 +0800

    feat(project): fix build error

--- vue/vite-naiveui/components.d.ts
+++ vue/vite-naiveui/components.d.ts
@@ -8,14 +8,12 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
-    NButton: typeof import('naive-ui')['NButton']
     NButtonGroup: typeof import('naive-ui')['NButtonGroup']
     NCard: typeof import('naive-ui')['NCard']
     NFlex: typeof import('naive-ui')['NFlex']
     NGi: typeof import('naive-ui')['NGi']
     NGrid: typeof import('naive-ui')['NGrid']
     NLayout: typeof import('naive-ui')['NLayout']
-    NSwitch: typeof import('naive-ui')['NSwitch']
     Reactivity: typeof import('./src/components/Reactivity.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
