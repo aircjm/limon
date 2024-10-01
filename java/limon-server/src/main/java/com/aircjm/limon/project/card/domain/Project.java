4592f30493c20539bb0ca64c58733e70d2f83fce
aircjm <aircjm@gmail.com>
2024-10-01 10:59:32 +0800

    add project

---
+++ java/limon-server/src/main/java/com/aircjm/limon/project/card/domain/Project.java
@@ -0,0 +1,54 @@
+package com.aircjm.limon.project.card.domain;
+
+import com.aircjm.limon.system.vo.DbBaseEntity;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@EqualsAndHashCode(callSuper = true)
+@Schema(name = "项目")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@TableName(value = "lm_project")
+public class Project extends DbBaseEntity<Project> {
+    /**
+     * id
+     */
+    @Schema(name = "id")
+    private Long id;
+
+    /**
+     * 标题
+     */
+    @Schema(name = "name")
+    private String name;
+
+    /**
+     * 内容的html
+     */
+    @Schema(name = "内容的html")
+    private String descHtml;
+
+    /**
+     * 内容的md
+     */
+    @Schema(name = "内容的md")
+    private String descMd;
+
+    /**
+     * 描述的json
+     */
+    @Schema(name = "描述的json")
+    private String descJson;
+
+    /**
+     * 状态
+     */
+    @Schema(name = "状态")
+    private Integer projectStatus;
+}
+
